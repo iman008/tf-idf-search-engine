840872c87b6adfc739abc7dc686970c641c47b9c
Imanghader <49917769+iman008@users.noreply.github.com>
2023-12-17 20:30:16 +0330

    ای ماه زهر هلاهل\n باز آ که زنگ ها ثلاثه \n

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-first load the dataset into the `data/` folder
+first load the dataset into the `data/` folder.
 
-then go to the root of project and run the following commands 
+then go to the root of project and run the following commands
+
 ```bash
 cd src/
 pypy3 wordlist_generator.py
 pypy3 tfidf_per_doc.py
+pypy3 main.py "{location of data.json}"
 ```
 
 now continue to work on the project.
--- src/main.py
+++ src/main.py
@@ -1,24 +1,47 @@
+import sys
 from search import search
+import json
+from termcolor import colored
+from time import time
 
-print(search("8 mg on the first day and 16 mg (the full dosage) on the second day.",[
-            23730,
-            25430,
-            47711,
-            41606,
-            48458,
-            12843,
-            20413,
-            46159,
-            46922,
-            18269,
-            4410,
-            5951,
-            27379,
-            34790,
-            6700,
-            33511,
-            12814,
-            8261,
-            23230,
-            22
-        ]))
+if len(sys.argv) != 2:
+    print("Usage: python main.py <path_to_data_json>")
+    sys.exit(1)
+
+data_json_path = sys.argv[1]
+
+testcases = json.loads(open(data_json_path, encoding="UTF8").read())
+print(len(testcases))
+corrects = 0
+wrongs = 0
+wrong = []
+cnt = 0
+
+overal_time = 0
+
+for testcase in testcases:
+    cnt += 1
+    # if cnt > 500:
+    #     break
+    print("-------------------------------------------")
+    print("searching in doc number ", testcase["document_id"])
+    start = time()
+    ans = search(testcase["query"], testcase["candidate_documents_id"])
+    end = time()
+    overal_time += (end - start) * 1000
+
+    check = []
+    for i in range(5):
+        check.append(ans[i][0])
+
+    if int(testcase["document_id"]) in check:
+        corrects += 1
+        print(colored("PASSED", 'green'))
+    else:
+        wrongs += 1
+        wrong.append((testcase["document_id"], testcase["query"], int(ans[0][0])))
+        print(colored("WRONG", 'red'))
+    print("-------------------------------------------")
+
+print("the accuracy is: ", corrects / (corrects + wrongs) * 100)
+print(f"overall time consumed for {corrects + wrongs} docs: ", overal_time)
